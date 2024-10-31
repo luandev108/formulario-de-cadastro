ec8f67d66035a546f114d2ab2c68cd95ea44aeb8
luandev108 <luanvnascimennto@gmail.com>
2024-10-31 17:54:57 -0300

    Add files via upload

--- formulario de cadastro/main.js
+++ formulario de cadastro/main.js
@@ -9,7 +9,17 @@ $(document).ready(function() {
     
     })
 
-    $('#telefone').mask('(00) 00000-0000')
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(00) 00000-0000'
+    })  
+    
+    $('#cpf').mask('000-000-000-00', {
+        placeholder: '000-000-000-00'
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder: '000-000-000-00'
+    })
 
 
     $('form').validate({
@@ -18,7 +28,8 @@ $(document).ready(function() {
                 required: true
             },
             email: {
-                required: true
+                required: true,
+                email: true
             },
             telefone: {
                 required: true
@@ -42,10 +53,10 @@ $(document).ready(function() {
             endereçoCompleto: 'Por favor, insira o endereço correto',
             cep: 'Por favor, insira o CEP correto',
         },
-    
-        submitHandler: function(form) {
+        submitHandler: function(form) { 
+            console.log(form)
             alert(`obrigado pelas informações, entraremos em contato`)
-            
+            form.reset();
         },
         invalidHandler: function(evento, validador) {
             let camposIncorreto = validador.numberOfInvalids();
